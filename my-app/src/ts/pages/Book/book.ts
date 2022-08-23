a7321bd6165443d47b99ccdfe77f7fcc2461037e
Oksana Astapova <astapova.frontend@gmail.com>
2022-08-24 00:38:57 +0300

    feat: add changing pagination and level range, saving the language while shifting

--- my-app/src/ts/pages/Book/book.ts
+++ my-app/src/ts/pages/Book/book.ts
@@ -1,12 +1,13 @@
 import Component from '../../template/Component';
 import Page from '../../template/Page';
-import { createWords } from './words';
+import { createBookContent, renderWords } from './create-content';
 
 class BookPage extends Page {
   render() {
     
-    createWords()
-
+    createBookContent()
+    renderWords(0,0, 'appear', 'hide');
+    
     document.body.className = 'body';
     return new Component('h1', 'book-title', '\n Book').node;
   }
--- my-app/src/ts/pages/Book/utils.ts
+++ my-app/src/ts/pages/Book/utils.ts
@@ -1,36 +1,135 @@
+import { renderWords, card_wrapper, pagination } from "./create-content";
+import { intoEn, intoRu } from "./switch-language";
 
-export const switchLang = () => {
+declare global {
+    interface Window { switchNextPage: any; }
+    interface Window { switchPrevPage: any; }
+}
 
-    const enButton = document.querySelector('.en') as HTMLButtonElement;
+export const switchLevels = () =>{
+
+    const levels = document.querySelector('.left-panel') as HTMLElement;
+
+    levels.addEventListener('click', (e)=>{
+        const target = e.target as HTMLElement;
+
+        const buttons = document.querySelectorAll('.left-panel button')
+        buttons.forEach(button => {
+            button.classList.remove('active');
+        })
+        target.classList.add('active');
+
+        const level = +(target.id);
+
+        card_wrapper.innerHTML = '';
+
+        const page = document.querySelector('.page-number') as HTMLElement;
+        page.innerHTML = '1';
+
+        const ruButton = document.querySelector('.ru') as HTMLButtonElement;
+
+        let en: string = `appear`
+        let ru: string = `hide`
+
+        if(ruButton.disabled == true){
+            ru = `appear`;
+            en = `hide`;
+        }
+        renderWords(0, level, en, ru);
+        
+    })
+
+    
+}
+
+window.switchNextPage = switchNextPage;
+window.switchPrevPage = switchPrevPage;
+
+let counter = 0;
+
+function switchNextPage(){
+
+    const prev = pagination.firstChild as HTMLButtonElement;
+    const next = pagination.lastChild as HTMLButtonElement;
+
+    
+    prev.disabled = false;
+
+    counter++;
+
+    card_wrapper.innerHTML = '';
+
+    const level_panel = document.querySelector('.left-panel') as HTMLElement;
+    const levels = level_panel.children;
+    
+    let level: number = 0;
+    for (let i = 0; i<levels.length; i++){
+        if(levels[i].classList.contains('active')){
+                    level = +(levels[i].id);
+                }
+    }
+    console.log(level)
+
+    const page = document.querySelector('.page-number') as HTMLElement;
+
+    let page_num = counter+1
+    page.innerHTML = page_num.toString();
+
+    if( counter == 29 ){
+        next.disabled = true;
+    }
+
     const ruButton = document.querySelector('.ru') as HTMLButtonElement;
-    const descr_ru = document.querySelectorAll('.description-ru');
-    const descr_en = document.querySelectorAll('.description-eng');
 
-    ruButton.addEventListener('click', ()=>{
-        
-        ruButton.disabled = true;
-        enButton.disabled = false;
+    let en: string = `appear`
+        let ru: string = `hide`
 
-        descr_ru.forEach(i =>{
-            i.classList.remove('hide');
-        })
+        if(ruButton.disabled == true){
+            ru = `appear`;
+            en = `hide`;
+        }
 
-        descr_en.forEach(i =>{
-            i.classList.add('hide');
-        })
-    })
+        renderWords(counter, level, en, ru);
+    
+}
 
-    enButton.addEventListener('click', ()=>{
-        
-        ruButton.disabled = false;
-        enButton.disabled = true;
+function switchPrevPage(){
 
-        descr_ru.forEach(i =>{
-            i.classList.add('hide');
-        })
+    const prev = pagination.firstChild as HTMLButtonElement;
 
-        descr_en.forEach(i =>{
-            i.classList.remove('hide');
-        })
-    })
+    counter--;
+
+    const level_panel = document.querySelector('.left-panel') as HTMLElement;
+    const levels = level_panel.children;
+    
+    let level: number = 0;
+    for (let i = 0; i<levels.length; i++){
+        if(levels[i].classList.contains('active')){
+                    level = +(levels[i].id);
+                }
+    }
+    console.log(level)
+
+    card_wrapper.innerHTML = '';
+
+    const page = document.querySelector('.page-number') as HTMLElement;
+
+    let page_num = counter+1
+    page.innerHTML = page_num.toString();
+
+    if( counter == 0 ){
+        prev.disabled = true;
+    }
+
+    const ruButton = document.querySelector('.ru') as HTMLButtonElement;
+
+    let en: string = `appear`
+        let ru: string = `hide`
+
+        if(ruButton.disabled == true){
+            ru = `appear`;
+            en = `hide`;
+        }
+
+    renderWords(counter, level, en, ru);
 }
