c88cb4771b1abf1ed3a14563fa49968ba24165be
dsh91698 <mettus@mail.ru>
2022-08-25 03:06:08 +0400

    fix: clearing body.wrapper element before adding our_team card

--- my-app/src/ts/pages/AboutTeam/AboutTeam.ts
+++ my-app/src/ts/pages/AboutTeam/AboutTeam.ts
@@ -2,12 +2,13 @@ import Component from "../../template/Component";
 import Page from "../../template/Page";
 import { ourTeam } from "./our_team";
 
-const wrapper = document.body.querySelector("#main__root");
+const wrapper = document.body.querySelector("#main__root") as HTMLElement;
 
 class AboutPage extends Page {
   render() {
     document.body.className = "body";
-    (wrapper as HTMLElement).append(ourTeam);
+    wrapper.innerHTML = ""; // clearing the wrapper element
+    wrapper.append(ourTeam);
     return new Component("", "", "").node;
     // return new Component("p", "", "\n About page").node;
   }
