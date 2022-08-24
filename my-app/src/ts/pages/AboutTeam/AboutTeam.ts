764c17e4fea7cd6af3f2a41461da84fa7c23bdc6
dsh91698 <mettus@mail.ru>
2022-08-25 01:59:32 +0400

    fix: our_team add

--- my-app/src/ts/pages/AboutTeam/AboutTeam.ts
+++ my-app/src/ts/pages/AboutTeam/AboutTeam.ts
@@ -2,13 +2,13 @@ import Component from "../../template/Component";
 import Page from "../../template/Page";
 import { ourTeam } from "./our_team";
 
-const wrapper = document.body.querySelector(".wrapper");
+const wrapper = document.body.querySelector("#main__root");
 
 class AboutPage extends Page {
   render() {
     document.body.className = "body";
     (wrapper as HTMLElement).append(ourTeam);
-    return new Component("p", "", "").node;
+    return new Component("", "", "").node;
     // return new Component("p", "", "\n About page").node;
   }
 }
