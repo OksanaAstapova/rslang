9848eb1c5fcd13bcbed16269ad154f9c034ee070
dsh91698 <mettus@mail.ru>
2022-08-28 21:54:13 +0400

    feat: level chise add, _scss file add

--- my-app/src/ts/pages/Audiocall/audiocall-game.ts
+++ my-app/src/ts/pages/Audiocall/audiocall-game.ts
@@ -1,22 +1,75 @@
 import * as helpers from "../AboutTeam/helpers";
 
-export const audiocall_game = ``;
+// export const audiocall_game = ``;
+
+const dropdownChoise = `
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+      <span>Выберите сложность</span>
+      <span class="icon is-small">
+        <i class="fa-solid fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu" role="menu">
+    <div class="dropdown-content">
+      <a href="#" class="dropdown-item">
+        Dropdown item
+      </a>
+      <a class="dropdown-item">
+        Other dropdown item
+      </a>
+      <a href="#" class="dropdown-item is-active">
+        Active dropdown item
+      </a>
+      <a href="#" class="dropdown-item">
+        Other dropdown item
+      </a>
+      <hr class="dropdown-divider">
+      <a href="#" class="dropdown-item">
+        With a divider
+      </a>
+    </div>
+</div>`
+
+
 
 const audiocallGameFunc = () => {
-  // const card = helpers.createHtmlElement("div", "team-card", "Наша команда");
-  const card = helpers.createHtmlElement("div", "team-card", "");
-  const teamHeader = helpers.createHtmlElement(
+  const card = helpers.createHtmlElement("div", "audio-game__card", "");
+  // just header with name ----------------------------------------
+  const audioHeader = helpers.createHtmlElement(
     "h1",
-    "team-header",
+    "audio-header",
     "Игра: Аудиовызов"
   );
-  card.append(teamHeader);
-  const membersWrapper = helpers.createHtmlElement(
+  card.append(audioHeader);
+  // --------------------------------------------------------------
+
+  //-----------------------------------------------------------------
+  const gameLegvelHeader = helpers.createHtmlElement(
+    "h2",
+    "audio-header",
+    "Выберите сложность"
+  );
+  card.append(gameLegvelHeader);
+  // --------------------------------------------------------------
+  const levelChoise = helpers.createHtmlElement(
     "div",
-    "members-wrapper",
+    "dropdown is-hoverable",
+    dropdownChoise
+  );
+  card.append(levelChoise);
+  // --------------------------------------------------------------
+
+  const audioWrapper = helpers.createHtmlElement(
+    "div",
+    "audio-game__wrapper",
     ""
   );
-  card.append(membersWrapper);
+  card.append(audioWrapper);
+  // --------------------------------------------------------------
+  // --------------------------------------------------------------
+  
   return card;
 };
 
