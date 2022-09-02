79a73164b3597e4ab6052fbeb0cd5c4b8717cb7b
dsh91698 <mettus@mail.ru>
2022-09-02 22:54:58 +0400

    feat: card create, sound play onLoad and onclick

--- my-app/src/ts/pages/Audiocall/Audiocall.ts
+++ my-app/src/ts/pages/Audiocall/Audiocall.ts
@@ -2,8 +2,7 @@ import Component from "../../template/Component";
 import Page from "../../template/Page";
 import { audiocallGame } from "./audiocall-game";
 import { root } from '../../router';
-import { randomPage, getWords } from "./get-words";
-
+import { randomPage, getWords, createAudiocallCard } from "./get-words";
 
 const wrapper = document.body.querySelector("#main__root") as HTMLElement;
 
@@ -18,7 +17,9 @@ class AudiocallPage extends Page {
         const level = (event.target as HTMLElement).innerHTML.split(" ")[0]; // get the level of words - from 0 to 5
         const group = randomPage(); // randomize page number in audiocall
         const levelWords = getWords(group, Number(level)).then(
-          (q: any) => { console.log("getWords->", q) }
+          (q: any) => {
+            console.log("getWords->", q);
+            createAudiocallCard(q)}
         );
       })
     });
--- my-app/src/ts/pages/Audiocall/get-words.ts
+++ my-app/src/ts/pages/Audiocall/get-words.ts
@@ -1,4 +1,6 @@
 // helpers audiocall
+import * as helpers from "../AboutTeam/helpers";
+import { root } from '../../router';
 
 export const getWords: any = async (page = 0, group = 0) => {
   const rawResponse = await fetch(
@@ -15,7 +17,56 @@ export const getWords: any = async (page = 0, group = 0) => {
 
 export const randomPage = () => {
   // from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-  return Math.floor(Math.random() * 6) + 1;
+  return Math.floor(Math.random() * 6) + 0.5;
 };
 
+export const createAudiocallCard: any = async (words: any) => {
+
+  function randomIntFromInterval(min = 1, max = 20) { // min and max included
+      // from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  };
+
+  const cardWordsSet = new Set();
+  while (cardWordsSet.size < 6) {
+    const randomWord = words[randomIntFromInterval()]
+    cardWordsSet.add(randomWord);
+  }
+  const cardWordsArr = Array.from(cardWordsSet);
+
+  console.log("words for card randome->", (cardWordsArr[1] as any).word);
+
+  const audio = new Audio(`https://rssslang.herokuapp.com/${(cardWordsArr[0] as any).audio}`);
+  audio.play(); // play the sound on first load card
+
+  const cardWord = `<div class="card">
+  <header class="card-header" onclick=audio.play()>
+  <audio id='audio' src='https://rssslang.herokuapp.com/${(cardWordsArr[0] as any).audio}'></audio>
+    <p class="card-header-title">
+      Choose the right word
+    </p>
+    <button class="card-header-icon" aria-label="sound">
+      <span class="icon">
+        <i class="fas fa-volume-up" aria-hidden="true"></i>
+      </span>
+    </button>
+  </header>
+  <div class="card-content">
+    <div class="content">
+      Click the sound icon to hear the word again.
+    </div>
+  </div>
+  <footer class="card-footer">
+    <a href="#" class="card-footer-item">${(cardWordsArr[1] as any).wordTranslate}</a>
+    <a href="#" class="card-footer-item">${(cardWordsArr[2] as any).wordTranslate}</a>
+    <a href="#" class="card-footer-item">${(cardWordsArr[3] as any).wordTranslate}</a>
+    <a href="#" class="card-footer-item">${(cardWordsArr[4] as any).wordTranslate}</a>
+    <a href="#" class="card-footer-item">${(cardWordsArr[0] as any).wordTranslate}</a>
+  </footer>
+</div>
+`;
+  const card = helpers.createHtmlElement("div", "", cardWord);
+  root.append(card);
+  return card;
+}
 
