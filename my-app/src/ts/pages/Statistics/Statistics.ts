dc5624963d7fbf4c9e46bab06afc5240e3af83c3
Clukva <yura4-1-1@mail.ru>
2022-09-04 19:34:09 +0300

    feat: add view statistic page

--- my-app/src/ts/pages/Statistics/Statistics.ts
+++ my-app/src/ts/pages/Statistics/Statistics.ts
@@ -11,19 +11,19 @@ class SprintPage extends Page {
     statisticDay.append(statisticDayTitle, statisticDayIn);
     const learnedWords = new Component("div", "statistics__day-words", "").node;
     const correctAnswear = new Component("div", "statistics__day-answear", "").node;
-    statisticDay.append(learnedWords, correctAnswear);
+    statisticDayIn.append(learnedWords, correctAnswear);
 
     const learnedWordsNumber = new Component("b", "day-words__number", "0").node;
     const learnedWordsText = new Component("p", "day-words__text", "Words learned").node;
     learnedWords.append(learnedWordsNumber, learnedWordsText);
 
-    const correctAnswearNumber = new Component("b", "day-words__number", "0").node;
+    const correctAnswearNumber = new Component("b", "day-answear__number", "0").node;
     const correctAnswearText = new Component("p", "day-words__text", "Correct answers").node;
     correctAnswear.append(correctAnswearNumber, correctAnswearText);
 
     const statisticGames = new Component("div", "statistics__games", "").node;
     const statisticGamesTitle = new Component("h2", "statistics__games-title", "Game stats").node;
-    const statisticGamesIn = new Component("div", "statistics__day-in", "").node;
+    const statisticGamesIn = new Component("div", "statistics__game-in", "").node;
     statisticGames.append(statisticGamesTitle, statisticGamesIn);
 
     const gameSprint = new Component("div", "statistics__sprint", "").node;
@@ -40,7 +40,7 @@ class SprintPage extends Page {
     const gameAudiocallLeaned = new Component("p", "game__audiocall-leaned", "✓ 0 words learned").node;
     const gameAudiocallCorrect = new Component("p", "game__audiocall-correct", "✓ Сorrect answers 0 %").node;
     const gameAudiocallLong = new Component("p", "game__audiocall-long", "✓ The longest series of correct answers: 0").node;
-    gameSprint.append(gameAudiocallTitle, gameAudiocallLeaned, gameAudiocallCorrect, gameAudiocallLong);
+    gameAudioCall.append(gameAudiocallTitle, gameAudiocallLeaned, gameAudiocallCorrect, gameAudiocallLong);
 
     statisticConteiner.append(statisticDay, statisticGames);
 
